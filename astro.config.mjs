ce126814c492e8c5a4b961253443587f36720f12
3333444n <134177546+3333444n@users.noreply.github.com>
2025-06-15 22:09:06 -0600

    feat: enable vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false, // Let us control base styles
-    })
+    }),
   ],
+  adapter: vercel({
+    imageService: true,
+  }),
+  experimental: {
+    responsiveImages: true,
+  },
   vite: {
     css: {
       devSourcemap: true,
@@ -17,6 +24,5 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'auto',
   },
-  output: 'static',
   site: 'https://your-site.com', // Update this with your actual site URL
 });
